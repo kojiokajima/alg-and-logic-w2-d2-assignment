4c44b19f817e6591afd9dff4ecede1f32c320a96
Aidan Middleton <aidanemiddleton@gmail.com>
2020-09-22 09:05:12 -0700

    first commit

---
+++ binarySearch.js
@@ -0,0 +1,16 @@
+// Write an algorithm that performs binary search on a given array.
+// the function will also track, and print out the number of steps required to reach the target
+// the index of the item 
+
+let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 29, 27, 26]
+
+const binarySearch = function(arr, target) {
+
+};
+
+
+
+
+console.log(binarySearch(testArray, 19));
+console.log(binarySearch(testArray, 12));
+console.log(binarySearch(testArray, 26));
---
+++ linearSearch.js
@@ -0,0 +1,15 @@
+// Write an algorithm that performs linear search on a given array.
+
+let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 29, 27, 26]
+
+const linearSearch = function(arr, target) {
+
+};
+
+
+
+
+console.log(linearSearch(testArray, 19));
+console.log(linearSearch(testArray, 12));
+console.log(linearSearch(testArray, 26));
+
