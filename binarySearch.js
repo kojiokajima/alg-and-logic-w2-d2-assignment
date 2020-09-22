f0d9a91812ee2e58d7e222c76786d0241caeab82
wanko <kouji124@w4.dion.ne.jp>
2020-09-23 02:59:50 +0900

    work comleted

--- binarySearch.js
+++ binarySearch.js
@@ -1,12 +1,32 @@
+// Eduardo, Miyu, and Koji
 // Write an algorithm that performs binary search on a given array.
 // the function will also track, and print out the number of steps required to reach the target
 // the index of the item 
 
-let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 29, 27, 26]
+let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 26, 27, 29];
 
-const binarySearch = function(arr, target) {
+const binarySearch = function (arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  let middle = (left + right) / 2;
+  let steps = 0;
 
+  while (arr[middle] !== target) {
+    // console.log(middle);
+
+    if(arr[middle] < target) {
+      left = middle;
+    } else if(arr[middle] > target) {
+      right = middle;
+    } else {
+      return middle;
+    }
+    middle = (left + right) / 2;
+    steps++;
+  }
+  return `It's ${middle}st/nd/rd/th number and takes ${steps} steps.`;
 };
+
 
 
 
@@ -14,3 +34,13 @@ const binarySearch = function(arr, target) {
 console.log(binarySearch(testArray, 19));
 console.log(binarySearch(testArray, 12));
 console.log(binarySearch(testArray, 26));
+
+// find the middlepoint
+
+// let middle =
+
+//   console.log(arr[middle]);
+
+//loop
+// if(testArry[middle] !== target) {
+// }
--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,16 @@
+// Eduardo, Miyu, and Koji.
 // Write an algorithm that performs linear search on a given array.
 
 let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 29, 27, 26]
 
 const linearSearch = function(arr, target) {
-
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === target) {
+      return `The index is ${i}.`;
+    } else {
+      ;
+    }
+  }
 };
 
 
